e281ad686a6c24699aab79176584b5e78eef25ef
lanson647 <lanson647@163.com>
2018-10-30 23:04:35 +0800

    修改了路径问题

--- demo.py
+++ demo.py
@@ -2,27 +2,19 @@ import itchat, time
 from itchat.content import *
 import re
 
-# @itchat.msg_register([TEXT, MAP, CARD, NOTE, SHARING])
-# def text_reply(msg):
-#     print('%s: %s' % (msg.type, msg.text))
 
-
-def note():
-    @itchat.msg_register(NOTE, isGroupChat=True)
-    def deal_note(msg):
-        try:
-            pattern = r'邀请"(.*?)"加入了群聊'
-            r = re.compile(pattern)
-            result = r.findall(msg['Content'])[0]
-            msg.user.send(u'''@{},欢迎您的加入! 谢谢关注！
+@itchat.msg_register(NOTE, isGroupChat=True)
+def deal_note(msg):
+    m = re.search('加入',msg.Content)
+    #直接截取第三个引号后的字符串
+    result = msg.text.split('"')[3]
+    if m is not None:
+        msg.user.send(u'''@{},欢迎您的加入! 谢谢关注！
 品牌工厂店直销，超级特卖，长按下图识别二维码，进入相册随意挑选哈。看中的请私信截图给@A萌萌 品牌工厂店直销'''.format(result))
-            msg.user.send('@img@%s' % 'pic.jpg')
-            time.sleep( 5 )
-        except Exception as e:
-            print(e)
+        msg.user.send('@img@%s' % 'xcx.png')
+    else:
+        print("失败了")
 
 
-if __name__ == '__main__':
-    note()
-    itchat.auto_login(hotReload=True)
-    itchat.run()
+itchat.auto_login(hotReload=True)
+itchat.run()
